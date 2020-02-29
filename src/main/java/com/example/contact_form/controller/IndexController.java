154166e05b1b5c18ccc400cfd51abad74724e8b4
kymx1983 <kymx1983@gmail.com>
2020-02-29 23:13:06 +0900

    no message

--- src/main/java/com/example/contact_form/controller/IndexController.java
+++ src/main/java/com/example/contact_form/controller/IndexController.java
@@ -15,14 +15,22 @@ import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * IndexController
+ */
 @Controller
 public class IndexController {
 
     @Autowired
     ContactRepository repository;
 
+    /**
+     * お問い合わせの一覧を表示する
+     *
+     * @return model
+     */
     @RequestMapping("/")
-    public ModelAndView index(){
+    public ModelAndView index() {
         ModelAndView model = new ModelAndView();
         model.setViewName("index");
 
@@ -32,33 +40,12 @@ public class IndexController {
         return model;
     }
 
-    @RequestMapping(value="/", method=RequestMethod.POST)
-    @Transactional(readOnly=false)
-    public ModelAndView create(@ModelAttribute("contact") @Validated Contact contact, BindingResult result){
-
-        if(result.hasErrors()) {
-            System.out.println("エラーが出ていますよ！！");
-            for(FieldError err: result.getFieldErrors()) {
-                System.out.println(err.getDefaultMessage());
-            }
-            ModelAndView model = new ModelAndView();
-            model.setViewName("edit");
-            model.addObject("contact", contact);
-            return model;
-        } else {
-            System.out.println("正常ですよ");
-            if( contact.getId() == 0){
-                // 新規作成の場合、ステータスを初期設定する
-                contact.setStatus("未着手");
-            }
-            contact.setCreatedAt(new Timestamp(System.currentTimeMillis()));
-            repository.saveAndFlush(contact);
-            return new ModelAndView("redirect:/");
-        }
-    }
-
+    /**
+     * 更新画面を表示する（登録時）
+     * @return
+     */
     @RequestMapping("/new")
-    public ModelAndView newContact(){
+    public ModelAndView edit() {
         ModelAndView model = new ModelAndView();
         model.setViewName("edit");
         model.addObject("contact", new Contact());
@@ -66,9 +53,13 @@ public class IndexController {
     }
 
 
-
+    /**
+     * 更新画面を表示する（更新時）
+     * @param id
+     * @return
+     */
     @RequestMapping("/edit")
-    public ModelAndView edit(@RequestParam long id){
+    public ModelAndView edit(@RequestParam long id) {
         ModelAndView model = new ModelAndView();
         model.setViewName("edit");
 
@@ -78,8 +69,43 @@ public class IndexController {
         return model;
     }
 
+    /**
+     * お問い合わせの新規作成及び更新を行う
+     *
+     * @param contact
+     * @param result
+     * @return
+     */
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @Transactional(readOnly = false)
+    public ModelAndView create(@ModelAttribute("contact") @Validated Contact contact, BindingResult result) {
+
+        if (result.hasErrors()) {
+            for (FieldError err : result.getFieldErrors()) {
+                System.out.println(err.getDefaultMessage());
+            }
+            ModelAndView model = new ModelAndView();
+            model.setViewName("edit");
+            model.addObject("contact", contact);
+            return model;
+        } else {
+            if (contact.getId() == 0) {
+                // 新規作成の場合、ステータスを初期設定する
+                contact.setStatus("未着手");
+            }
+            contact.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+            repository.saveAndFlush(contact);
+            return new ModelAndView("redirect:/");
+        }
+    }
+
+    /**
+     * お問い合わせの削除を行う
+     * @param id
+     * @return
+     */
     @RequestMapping("/delete")
-    public ModelAndView delete(@RequestParam long id){
+    public ModelAndView delete(@RequestParam long id) {
         System.out.println("削除");
         ModelAndView model = new ModelAndView();
         repository.deleteById(id);
